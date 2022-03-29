a32bfd6adc32301ad56115fe9a97e4386142f49e
Gelmoxx <djtoso@gmail.com>
2022-03-29 22:03:44 +0200

    Funkčný backend pre Subject

--- src/main/java/com/example/demo/SubjectController.java
+++ src/main/java/com/example/demo/SubjectController.java
@@ -18,25 +18,21 @@ public class SubjectController {
         return subjectService.getSubjects(subjectName);
     }
 
-    //Hľadanie knihy podľa ID
     @GetMapping("/api/subjects/{subjectId}")
     public SubjectDto getSubjectsById(@PathVariable Long subjectId){
         return subjectService.getSubjectById(subjectId);
     }
 
-    //Pridanie novej knihy
     @PostMapping("/api/subjects")
     public Long createSubject(@RequestBody SubjectDto subjectDto){
        return subjectService.createSubject(subjectDto);
     }
 
-    //Zmazanie knihy podľa ID
     @DeleteMapping("/api/subjects/{subjectId}")
     public void deleteSubject(@PathVariable Long subjectId){
         subjectService.deleteSubject(subjectId);
     }
 
-    //Aktualizácia knihy podľa ID
     @PutMapping("/api/subjects/{subjectId}")
     public void updateSubject(@PathVariable Long subjectId, @RequestBody SubjectDto subjectDto){
         subjectService.updateSubject(subjectId, subjectDto);
